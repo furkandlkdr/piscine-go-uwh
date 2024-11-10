912809ee723c7b3fb18f33b52aa88f924b22e3e6
Furkan D <furkandlkdr@hotmail.com>
2024-11-11 02:24:12 +0300

    Added final quest 7

--- quest-7/13-list-sort.go
+++ quest-7/13-list-sort.go
@@ -8,5 +8,17 @@ type NodeI struct {
 //Write a function ListSort that sorts the nodes of a linked list by ascending order.
 
 func ListSort(l *NodeI) *NodeI {
-
+	if l == nil {
+		return nil
+	}
+	var current *NodeI
+	var next *NodeI
+	for current = l; current != nil; current = current.Next {
+		for next = current.Next; next != nil; next = next.Next {
+			if current.Data > next.Data {
+				current.Data, next.Data = next.Data, current.Data
+			}
+		}
+	}
+	return l
 }
